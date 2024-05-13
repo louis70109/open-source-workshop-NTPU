2cf45753989483444d0c0f1da46cf04e5dad7e54
Calvin Wu <calvin541024@gmail.com>
2024-05-13 19:56:32 +0800

    Update main.py

--- main.py
+++ main.py
@@ -1,13 +1,8 @@
-from linebot.v3 import (
-    WebhookHandler
+from linebot import (
+    LineBotApi, WebhookHandler
 )
-from linebot.v3.messaging import (
-    Configuration,
-    ApiClient,
-    MessagingApi,
-    ReplyMessageRequest,
-    TextMessage,
-    ShowLoadingAnimationRequest
+from linebot.models import (
+    TextSendMessage
 )
 import json
 import os
@@ -17,26 +12,20 @@ from io import BytesIO
 from firebase import firebase
 import google.generativeai as genai
 
-
 # 使用環境變量讀取憑證
 secret = os.getenv('ChannelSecret', None)
 token = os.getenv('ChannelAccessToken', None)
-# firebase_url = os.getenv('FIREBASE_URL')
-
 
 handler = WebhookHandler(secret)
-configuration = Configuration(
-    access_token=token
-)
-
+line_bot_api = LineBotApi(token)
 
 def linebot(request):
     body = request.get_data(as_text=True)
     json_data = json.loads(body)
     try:
-
         signature = request.headers['X-Line-Signature']
         handler.handle(body, signature)
+
         event = json_data['events'][0]
         reply_token = event['replyToken']
         user_id = event['source']['userId']
@@ -44,35 +33,22 @@ def linebot(request):
 
         if msg_type == 'text':
             msg = event['message']['text']
+            if msg == '!清空':
+                reply_msg = '已清空'
+            elif msg == '!摘要':
+                reply_msg = msg # test
+            else:
+                reply_msg = "哈囉你好嗎"
 
-            with ApiClient(configuration) as api_client:
-                line_bot_api = MessagingApi(api_client)
-                line_bot_api.show_loading_animation(ShowLoadingAnimationRequest(
-                    chatId=user_id, loadingSeconds=20))
-
-                if msg == '!清空':
-                    reply_msg = '已清空'
-                    # fdb.delete(user_chat_path, None)
-                elif msg == '!摘要':
-                    reply_msg = msg # test
-                else:
-                    reply_msg = "哈囉你好嗎"
-
-                line_bot_api.reply_message(
-                    ReplyMessageRequest(
-                        reply_token=reply_token,
-                        messages=[
-                            TextMessage(text=reply_msg),
-                        ]))
+            line_bot_api.reply_message(
+                reply_token,
+                messages=[TextSendMessage(text=reply_msg)]
+            )
         else:
-            with ApiClient(configuration) as api_client:
-                line_bot_api = MessagingApi(api_client)
-                line_bot_api.reply_message(
-                    ReplyMessageRequest(
-                        reply_token=reply_token,
-                        messages=[
-                            TextMessage(text='你傳的不是文字訊息喔'),
-                        ]))
+            line_bot_api.reply_message(
+                reply_token,
+                messages=[TextSendMessage(text='你傳的不是文字訊息喔')]
+            )
 
     except Exception as e:
         detail = e.args[0]
